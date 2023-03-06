8a39406cab6462f80f942d4eec9841aa7aa1be29
German (forodie) <german.utkingu@gmail.com>
2023-03-06 03:07:28 +0300

    create faStr2

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,10 +2,10 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    int res = 0; // для подсчета слов
+    int res = 0; // res - для подсчета слов
     bool flag_1 = false, flag_2 = false; // flag_1 - если встретили цифру в слове, будет true 
                                          
-    for (int i = 0; str[i] != '\0'; i++) { // \0 - конец строки (пустой символ)
+    for (int i = 0; str[i] != '\0'; i++) { // пробегаем по строке,  \0 - конец строки (пустой символ)
         if ('0' <= str[i] && str[i] <= '9') { 
             flag_1 = true;
         }
@@ -27,7 +27,22 @@ unsigned int faStr1(const char *str) {
 }
 
 unsigned int faStr2(const char *str) {
-    int res = 0;
+    int res = 0; 
+    bool flagCorrect = false, flagSpace = true;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'A' && str[i] <= 'Z' && flagSpace != false) { flagCorrect = true; } // проверяем корректность слова 
+                                                                                          // заглавные буквы не должны встречаться после первой.
+        if (str[i] >= 'A' && str[i] <= 'z') {
+            flagSpace = false;
+        } 
+        else if (str[i] != ' ') { flagCorrect = false; } 
+        else {
+            if (flagCorrect == true) { res++; }
+            flagCorrect = false; // ставим флаги по дефолту
+            flagSpace = true; 
+        }
+    }
 
     return res;
 }
